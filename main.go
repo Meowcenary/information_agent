b0803e6856816541b75c834329a4208ccf57ad32
Eric Newman <newmaneric5@gmail.com>
2023-11-07 14:32:00 -0600

    Adding Simple HTML Page For Query Submission
    Getting a handle on how all of this works

--- main.go
+++ main.go
@@ -14,10 +14,18 @@ import (
 
 func main() {
   http.HandleFunc("/", handler)
+  http.HandleFunc("/submit", submitHandler)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// handler functions must have http.RepsonseWriter and *http.Request as function params
 func handler(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("root.html")
     t.Execute(w, nil)
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+  // handle the query here
+  t, _ := template.ParseFiles("query_results.html")
+  t.Execute(w, nil)
 }
